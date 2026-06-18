a33f6e20967c92497174dd8278d60ae76dfee24f
Md Quayem Ashraf <quayemanas@gmail.com>
2026-06-18 10:20:09 +0530

    configuration done

--- tweet-service/src/main/java/com/twitter/tweet/service/TweetServiceApplication.java
+++ tweet-service/src/main/java/com/twitter/tweet/service/TweetServiceApplication.java
@@ -2,8 +2,10 @@ package com.twitter.tweet.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TweetServiceApplication {
 
 	public static void main(String[] args) {
