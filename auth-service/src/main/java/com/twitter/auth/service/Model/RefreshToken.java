9cf9d90fe8908ddf6d3e090a81b086ba3ab8e01f
Md Quayem Ashraf <quayemanas@gmail.com>
2026-06-20 19:34:39 +0530

    Resolve RefreshToken merge conflict

--- auth-service/src/main/java/com/twitter/auth/service/Model/RefreshToken.java
+++ auth-service/src/main/java/com/twitter/auth/service/Model/RefreshToken.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,28 +16,18 @@ public class RefreshToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< Updated upstream
     private Long tokenId;
-=======
+
     private Long id;
->>>>>>> Stashed changes
 
     @Column(nullable = false, unique = true)
     private String token;
 
-<<<<<<< Updated upstream
-    private LocalDateTime expiryDate;
-
-=======
->>>>>>> Stashed changes
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-<<<<<<< Updated upstream
-
-=======
     @Column(nullable = false)
     private Instant expiryDate;
->>>>>>> Stashed changes
+
 }
